bcd75cd46b4037f7bea310ff4c9b17ae8b1ab1a5
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-04-10 20:52:04 +0300

    feat: created components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import MenuIcon from "@mui/icons-material/Menu";
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <div>
+        <span>
+          <MenuIcon />
+        </span>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
