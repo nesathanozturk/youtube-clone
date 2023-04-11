e072b8dcf78e3951c5f7b7b080dbf298139b905a
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-04-11 22:11:15 +0300

    style: edited sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,9 +3,7 @@ import SidebarBox from "./SidebarBox";
 const Sidebar = () => {
   return (
     <aside className="bg-youtube hidden xs:block xs:top-20 xs:z-50 xs:fixed xs:left-0 bottom-0 xs:w-[80px] xs:h-full 2xl:w-[15%]">
-      <div className="container">
-        <SidebarBox />
-      </div>
+      <SidebarBox />
     </aside>
   );
 };
