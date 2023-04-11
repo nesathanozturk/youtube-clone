8e0b80cd4a7a5be525865a55d6d0cac6a3039005
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-04-11 16:34:10 +0300

    chore: removed class names

---
+++ src/components/NavbarBox.jsx
@@ -0,0 +1,62 @@
+const NavbarBox = () => {
+  return (
+    <div className="w-full mt-6 hidden sm:flex items-center gap-3 ml-[70px] text-white text-sm font-medium pb-4 overflow-hidden">
+      <a
+        href=""
+        className="w-16 h-[1.8rem] bg-white text-center rounded-lg p-1 text-black"
+      >
+        Tümü
+      </a>
+      <a
+        href=""
+        className="w-16 h-[1.8rem] bg-[#272727] text-center rounded-lg pt-1 box-color transition-colors"
+      >
+        Mix'ler
+      </a>
+      <a
+        href=""
+        className="w-16 h-[1.8rem] bg-[#272727] text-center rounded-lg pt-1 box-color transition-colors"
+      >
+        Müzik
+      </a>
+      <a
+        href=""
+        className="w-16 h-[1.8rem] bg-[#272727] text-center rounded-lg pt-1 box-color transition-colors"
+      >
+        Oyun
+      </a>
+      <a
+        href=""
+        className="w-16 h-[1.8rem] bg-[#272727] text-center rounded-lg pt-1 box-color transition-colors"
+      >
+        Canlı
+      </a>
+      <a
+        href=""
+        className="w-16 h-[1.8rem] bg-[#272727] text-center rounded-lg pt-1 box-color transition-colors"
+      >
+        Futbol
+      </a>
+      <a
+        href=""
+        className="w-32 h-[1.8rem] bg-[#272727] text-center rounded-lg pt-1 box-color transition-colors"
+      >
+        Son Yüklenenler
+      </a>
+      <a
+        href=""
+        className="w-24 h-[1.8rem] bg-[#272727] text-center rounded-lg pt-1 box-color transition-colors"
+      >
+        İzlenenler
+      </a>
+      <a
+        href=""
+        className="w-24 h-[1.8rem] bg-[#272727] text-center rounded-lg pt-1 box-color transition-colors"
+      >
+        Yeni öneriler
+      </a>
+    </div>
+  );
+};
+
+export default NavbarBox;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,34 +5,6 @@ const Sidebar = () => {
     <aside className="bg-youtube hidden xs:block xs:top-20 xs:z-50 xs:fixed xs:left-0 bottom-0 xs:w-[80px] xs:h-full 2xl:w-[15%]">
       <div className="container">
         <SidebarBox />
-        <div className="hidden 2xl:flex text-[0.8rem] text-[#a4a4a4] font-medium flex-wrap gap-1 mb-4 p-1">
-          <a href="https://about.youtube/">Hakkında</a>
-          <a href="https://blog.youtube/">Basın</a>
-          <a href="https://www.youtube.com/howyoutubeworks/policies/copyright/">
-            Telif hakkı
-          </a>
-          <a href="https://www.youtube.com/t/contact_us/">Bize ulaşın</a>
-          <a href="https://www.youtube.com/creators/">İçerik Üreticiler</a>
-          <a href="https://www.youtube.com/ads/">Reklam verme</a>
-          <a href="https://developers.google.com/youtube?hl=tr">
-            Geliştiriciler
-          </a>
-        </div>
-        <div className="hidden 2xl:flex text-[0.8rem] text-[#a4a4a4] font-medium flex-wrap gap-1 mb-4 p-1">
-          <a href="https://www.youtube.com/t/terms">Şartlar</a>
-          <a href="https://policies.google.com/u/1/privacy?hl=tr">Gizlilik</a>
-          <br />
-          <a href="https://www.youtube.com/howyoutubeworks/policies/community-guidelines/">
-            Politika ve Güvenlik
-          </a>
-          <a href="https://www.youtube.com/howyoutubeworks/?utm_campaign=ytgen&utm_source=ythp&utm_medium=LeftNav&utm_content=txt&u=https%3A%2F%2Fwww.youtube.com%2Fhowyoutubeworks%3Futm_source%3Dythp%26utm_medium%3DLeftNav%26utm_campaign%3Dytgen">
-            YouTube Nasıl Çalışıyor?
-          </a>
-          <a href="https://www.youtube.com/new">Yeni özellikleri deneyin</a>
-        </div>
-        <p className="hidden 2xl:block text-[0.8rem] text-[#717171]">
-          &copy; 2023 Google LLC
-        </p>
       </div>
     </aside>
   );
