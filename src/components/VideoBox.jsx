68a0467a02ac32d628faab1d9b5aa246bd3d6e5c
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-04-11 17:24:22 +0300

    chore: deleted data

--- src/components/VideoBox.jsx
+++ src/components/VideoBox.jsx
@@ -5,26 +5,26 @@ const VideoBox = () => {
     <div className="w-full md:w-[320px] md:h-72 lg:w-[340px] lg:h-72">
       <div className="container group">
         <img
-          src="https://www.cumhuriyet.com.tr/Archive/2020/11/17/180930941-04dead3-superjumbo.jpg"
-          alt="Video"
+          src="https://i.ytimg.com/vi/yKNxeF4KMsY/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLC6cwBse6xhHEeiEzdadIsABYG4Xg"
+          alt="Coldplay - Yellow (Official Video)"
           className="w-full max-h-48 md:h-52 rounded-xl cursor-pointer"
         />
         <div className="w-full flex justify-between gap-2 mt-4">
           <div className="flex gap-2">
             <img
-              src="https://yt3.ggpht.com/yti/AHXOFjVUv9n4xuGAVyc0I0O3bdikXSIxvtL-uQ9lBA=s88-c-k-c0x00ffffff-no-rj-mo"
-              alt="Neşathan Öztürk"
+              src="https://yt3.ggpht.com/nCMHKdzJCDlE5uX_9HplknQdYgrKEs3yZMSQNx2yYs8gRA05NJAZQAe8TMNpna_EEIaKx6FR=s48-c-k-c0x00ffffff-no-nd-rj"
+              alt="Coldplay"
               className="max-h-6 max-w-6 md:max-w-8 md:max-h-8 rounded-full cursor-pointer"
             />
             <div>
               <h1 className="text-[0.8rem] sm:text-sm md:text-base text-white font-semibold">
-                (TWD) Here's Negan | Choice
+                Coldplay - Yellow
               </h1>
               <p className="text-[#b4b4b4] text-[0.6rem] sm:text-xs md:text-sm">
-                PyroSync
+                Coldplay
               </p>
               <p className="text-[#b4b4b4] text-[0.6rem] sm:text-xs md:text-sm">
-                70 B görüntüleme - 2 yıl önce
+                864 Mn görüntüleme 11 yıl önce
               </p>
             </div>
           </div>
